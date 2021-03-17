c866c676db80be7f7281b1e8878f0892efc6d7ec
Kiarash <ksabahi2001@gmail.com>
2021-03-17 06:02:46 +0330

    fixed routes problem

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,42 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route
-} from "react-router-dom";
-import './index.css';
-import "./assets/css/main.css"
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Switch, HashRouter as Router, Route } from "react-router-dom";
+import "./index.css";
+import "./assets/css/main.css";
+import reportWebVitals from "./reportWebVitals";
 import AllConnections from "./components/AllConnections";
 import SideMenu from "./components/SideMenu";
 import Main from "./components/Main";
 import NewConnection from "./components/NewConnection";
 import Header from "./components/Header";
+import ConnectionPage from "./components/ConnectionPage";
 
 ReactDOM.render(
-  <React.StrictMode>
-      <Router>
-          <div className="app">
-              <Header/>
-              <div className="parent">
-                  <SideMenu/>
-                  <Switch>
-                      <Route path="/connection/new">
-                          <Main component={NewConnection}/>
-                      </Route>
-                      <Route path="/">
-                          <Main component={AllConnections}/>
-                      </Route>
-                  </Switch>
-              </div>
-          </div>
-      </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			<div className="app">
+				<Header />
+				<div className="parent">
+					<SideMenu />
+					<Switch>
+						<Route path="/connection/new">
+							<Main component={NewConnection} />
+						</Route>
+						<Route path="/connection/fav">
+							<Main component={NewConnection} />
+						</Route>
+						<Route path="/connection/recent">
+							<Main component={NewConnection} />
+						</Route>
+						<Route path="/connection/:id" component={ConnectionPage} />
+						<Route path="/">
+							<Main component={AllConnections} />
+						</Route>
+						<Route component={AllConnections}/>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	</React.StrictMode>,
+	document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
