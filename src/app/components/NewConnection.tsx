11555f7d5f06a59a8aff7c0492f3937f95cbcb0a
Kiarash <ksabahi2001@gmail.com>
2021-03-24 17:40:51 +0430

    New connection page

--- src/app/components/NewConnection.tsx
+++ src/app/components/NewConnection.tsx
@@ -1,42 +1,88 @@
-import React from "react";
-import "../assets/css/newConnection.css"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from "react";
+import "../assets/css/newConnection.css";
+import { ipcRenderer } from "electron";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
+import { Dialog } from "@material-ui/core";
 
+type Inputs = {
+	name: string;
+	host: string;
+	port: string;
+	username: string;
+	password: string;
+};
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const NewConnection = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const [addConnectionStatus, setAddConnectionStatus] = useState("");
+	const { register, handleSubmit } = useForm<Inputs>();
+	const router = useHistory();
 
-    return <div id={"new-connection"}>
-        <form id={"new-connection-form"}>
-            <div id={"new-connection-title"}>
-                Add a new connection
-            </div>
-            <div id={"inputs"}>
-                <div className="input-wrapper">
-                    <div className={"input-label"}>Host</div>
-                    <input/>
-                </div>
-                <div className="input-wrapper">
-                    <div className={"input-label"}>Port</div>
-                    <input/>
-                </div>
-                <div className="input-wrapper">
-                    <div className={"input-label"}>Name</div>
-                    <input/>
-                </div>
-                <div className="input-wrapper">
-                    <div className={"input-label"}>Username</div>
-                    <input/>
-                </div>
-                <div className="input-wrapper">
-                    <div className={"input-label"}>Password</div>
-                    <input/>
-                </div>
-            </div>
-            <div id={"buttons"}>
-                <button id={"add-button"}>Add</button>
-            </div>
-        </form>
-    </div>
-}
+	const onSubmit = (data: Inputs) => {
+		ipcRenderer.send("connections:add", {
+			name: data.name,
+			address: data.host + data.port,
+			username: data.username,
+			password: data.password,
+		});
+		ipcRenderer.on("connections:add", (event, result, error) => {
+			setIsOpen(true);
+			if (error) {
+				setAddConnectionStatus(error.toString());
+			} else {
+				setAddConnectionStatus("Done");
+			}
+		});
+	};
 
-export default NewConnection
+	return (
+		<div id={"new-connection"}>
+			<form id={"new-connection-form"} onSubmit={handleSubmit(onSubmit)}>
+				<div id={"new-connection-title"}>Add a new connection</div>
+				<div id={"inputs"}>
+					<div className="input-wrapper">
+						<div className={"input-label noselect"}>Host</div>
+						<input name="host" ref={register} autoFocus={true} />
+					</div>
+					<div className="input-wrapper">
+						<div className={"input-label noselect"}>Port</div>
+						<input name="port" ref={register} />
+					</div>
+					<div className="input-wrapper">
+						<div className={"input-label noselect"}>Name</div>
+						<input name="name" ref={register} />
+					</div>
+					<div className="input-wrapper">
+						<div className={"input-label noselect"}>Username</div>
+						<input name="username" ref={register} />
+					</div>
+					<div className="input-wrapper">
+						<div className={"input-label noselect"}>Password</div>
+						<input name="password" ref={register} />
+					</div>
+				</div>
+				<div id={"buttons"}>
+					<button id={"add-button"} type={"submit"}>
+						Add
+					</button>
+				</div>
+			</form>
+			<Dialog open={isOpen}>
+				<div style={{ padding: 300 }}>
+					{addConnectionStatus}
+					<button
+						onClick={() => {
+							addConnectionStatus === "Done" ? router.push("/") : setIsOpen(false);
+						}}
+					>
+						ok
+					</button>
+				</div>
+			</Dialog>
+		</div>
+	);
+};
+
+export default NewConnection;
