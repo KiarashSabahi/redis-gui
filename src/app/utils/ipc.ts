1163efa1149d03522b3e6ef7c16796f4e31bcb43
Kiarash <ksabahi2001@gmail.com>
2021-03-26 16:55:55 +0430

    function for shortening ipc requests

---
+++ src/app/utils/ipc.ts
@@ -0,0 +1,18 @@
+import { ipcRenderer } from "electron";
+import { Connection } from "../types/Connection";
+
+export const ipcSend = async (event: string, input: any) => {
+	const ipcRendererSync = () =>
+		new Promise((resolve, reject) => {
+			const ipcListener = (event, item, err) => {
+				if (err) {
+					reject(err);
+				}
+				resolve(item);
+				ipcRenderer.removeListener(event, ipcListener);
+			};
+			ipcRenderer.on(event, ipcListener);
+		});
+	ipcRenderer.send(event, input);
+	return (await ipcRendererSync()) as Connection;
+};
